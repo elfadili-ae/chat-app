5916d2816c7d45615b95c42dc02cce8ac40921e9
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-09-09 21:15:08 +0000

    firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+//replace with your own firebase configuratin
 const firebaseConfig = {
-    apiKey: "AIzaSyAaV0WRXsRp-aJpmGA8K8c7Uwe9GVPb1vs",
-    authDomain: "chat-39b4b.firebaseapp.com",
-    projectId: "chat-39b4b",
-    storageBucket: "chat-39b4b.appspot.com",
-    messagingSenderId: "590620648328",
-    appId: "1:590620648328:web:c58f1c13640ec83842103d"
+    apiKey: "google_firebase_api_key",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storeBucket",
+    messagingSenderId: "msi",
+    appId: "app id"
 };
 
 export const app = initializeApp(firebaseConfig);
