11d51c191d7b6bd40acc932a7052ef8887768e12
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-09-10 11:29:13 +0000

    feat: display user's info in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth } from '../firebase'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const { currentUser } = useContext(AuthContext);
     return (
         <div className='navbar'>
             <h2 className='logo'>Chat App</h2>
             <div className='user'>
-                <img src='https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='profile picture' />
-                <p>John</p>
-                <button onClick={() => { signOut(auth) }}>Logout</button>
+                <img src={currentUser.photoURL} alt='profile picture' />
+                <p>{currentUser.displayName}</p>
+                <button onClick={() => { signOut(auth); navigate("/login") }}>Logout</button>
             </div>
         </div>
     )
