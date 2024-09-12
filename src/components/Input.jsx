442bc7e659edb5438ca84715a152ffb42d5dc051
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-09-12 11:59:56 +0000

    feat: display picture when selected

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useState } from 'react'
-import Attach from '../images/attachment.png'
+import Gallery from '../images/gallery.png'
 import { ChatContext } from '../context/ChatContext';
 import { AuthContext } from '../context/AuthContext';
 import { Timestamp, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 import { db } from '../firebase';
 import { v4 as uuid } from 'uuid';
+import { ref, uploadBytesResumable } from 'firebase/storage';
 
 const Input = () => {
     const { data } = useContext(ChatContext);
@@ -14,21 +15,65 @@ const Input = () => {
     const [image, setImage] = useState();
 
     const handleSend = async () => {
-        if (msg === "")
+        if (msg === "" && image === null)
             return;
         try {
-            await updateDoc(doc(db, "chats", data.chatID), {
-                messages: arrayUnion({
-                    id: uuid(),
-                    text: msg,
-                    sender: currentUser.uid,
-                    date: Timestamp.now(),
-                })
-            });
+            if (image) {
+
+                const storageRef = ref(storage, `${username}_${file.name}`);
+                const uploadTask = uploadBytesResumable(storageRef, file);
+                uploadTask.on(
+                    'state_changed', // Specify the event type
+                    (snapshot) => {
+                        // You can monitor the progress of the upload here if needed
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                    },
+                    (error) => {
+                        // Handle unsuccessful uploads
+                        console.error("Error during upload:", error.message);
+                        setErr(true);
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                            await updateDoc(doc(db, "chats", data.chatID), {
+                                messages: arrayUnion({
+                                    id: uuid(),
+                                    text: msg,
+                                    photoURL: downloadURL,
+                                    sender: currentUser.uid,
+                                    date: Timestamp.now(),
+                                })
+                            });
+                        });
+                    }
+                );
+            } else {
+                await updateDoc(doc(db, "chats", data.chatID), {
+                    messages: arrayUnion({
+                        id: uuid(),
+                        text: msg,
+                        sender: currentUser.uid,
+                        date: Timestamp.now(),
+                    })
+                });
+            }
         } catch (error) {
             console.log(error);
         }
         setMsg("");
+    }
+
+    const fileSelected = (e) => {
+        console.log("file selected");
+        e.preventDefault();
+        if (e.target.files.length > 0) {
+            console.log("change picture", e.target.files[0]);
+            setImage(e.target.files[0]);
+        } else {
+            setImage(null);
+        }
     }
     return (
         <div className='input'>
@@ -37,9 +82,9 @@ const Input = () => {
                 value={msg}
                 onChange={(e) => { setMsg(e.target.value) }} />
             <div className='actions'>
-                <input type='file' id='file' style={{ display: 'none' }} />
+                <input type='file' id='file' style={{ display: 'none' }} onChange={fileSelected} />
                 <label htmlFor='file'>
-                    <img src={Attach} alt='attach file' />
+                    <img src={image ? URL.createObjectURL(image) : Gallery} alt='attach file' />
                 </label>
                 <button onClick={() => { handleSend() }}>Send</button>
             </div>
