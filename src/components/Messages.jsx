f08528eb7b10f726e21451b86176adb552b9173b
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-09-13 18:41:01 +0000

    feat: scroll to latest message

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Message from './Message'
 import { ChatContext } from '../context/ChatContext'
 import { doc, onSnapshot } from 'firebase/firestore';
@@ -7,11 +7,19 @@ import { db } from '../firebase';
 const Messages = () => {
     const { data } = useContext(ChatContext);
     const [messages, setMessages] = useState([]);
+    const msgEnd = useRef();
+
+    const scrollToBottom = (behave = "smooth") => {
+        if (msgEnd.current) {
+            msgEnd.current.scrollIntoView({ behavior: behave });
+        }
+    }
 
     useEffect(() => {
         const getMessages = () => {
             const unsub = onSnapshot(doc(db, "chats", data.chatID), (doc) => {
                 doc.exists() && setMessages(doc.data().messages);
+                scrollToBottom();
             });
 
             return () => {
@@ -21,12 +29,16 @@ const Messages = () => {
         data.chatID && getMessages();
     }, [data.chatID]);
 
+    useEffect(() => {
+        scrollToBottom("auto");
+    }, [messages])
+
     return (
         <div className='messages'>
             {messages.map((element) => {
-                console.log("element", element);
                 return <Message message={element} key={element.id} />
             })}
+            <div ref={msgEnd} style={{ height: "20px" }}></div>
         </div>
     )
 }
