b332912f83e5d7dbd650b89e02698866268880e2
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-09-14 11:48:55 +0000

    feat: added logo in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,15 @@ import { auth } from '../firebase'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 import DefaultAvatar from '../images/defaultpic.png'
+import logo from '../images/chatlogo.png'
 
 const Navbar = () => {
     const navigate = useNavigate();
     const { currentUser } = useContext(AuthContext);
     return (
         <div className='navbar'>
-            <h2 className='logo'>Chat App</h2>
+            {/* <h2 className='logo'>Chat App</h2> */}
+            <img src={logo} alt='chat app logo' />
             <div className='user'>
                 <img src={currentUser.photoURL ? currentUser.photoURL : DefaultAvatar} alt='profile picture' />
                 <p>{currentUser.displayName}</p>
