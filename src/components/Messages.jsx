fc50e30bd26b7c1906abacb0e9ae863fdbce00bf
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-09-14 23:05:18 +0000

    feat: play sound when message received/sent

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -3,10 +3,15 @@ import Message from './Message'
 import { ChatContext } from '../context/ChatContext'
 import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase';
+import NewMessage from '../audio/newMessage.mp3';
+import MyMessage from '../audio/mymessage.wav';
+import { AuthContext } from '../context/AuthContext';
 
 const Messages = () => {
     const { data } = useContext(ChatContext);
+    const { currentUser } = useContext(AuthContext);
     const [messages, setMessages] = useState([]);
+    const isInitialLoad = useRef(true);
     const msgEnd = useRef();
 
     const scrollToBottom = (behave = "smooth") => {
@@ -20,6 +25,16 @@ const Messages = () => {
             const unsub = onSnapshot(doc(db, "chats", data.chatID), (doc) => {
                 doc.exists() && setMessages(doc.data().messages);
                 scrollToBottom();
+                if (!isInitialLoad.current) {
+                    if (doc.exists() && doc.data().messages[doc.data().messages.length - 1]?.sender !== currentUser.uid) {
+                        document.getElementById("new-message").play();
+                    }
+                    if (doc.exists() && doc.data().messages[doc.data().messages.length - 1]?.sender === currentUser.uid) {
+                        document.getElementById("my-message").play();
+                    }
+                } else {
+                    isInitialLoad.current = false;
+                }
             });
 
             return () => {
@@ -27,6 +42,10 @@ const Messages = () => {
             }
         }
         data.chatID && getMessages();
+
+        return () => {
+            isInitialLoad.current = true;
+        }
     }, [data.chatID]);
 
     useEffect(() => {
@@ -39,6 +58,8 @@ const Messages = () => {
                 return <Message message={element} key={element.id} />
             })}
             <div ref={msgEnd} style={{ height: "20px" }}></div>
+            <audio id='new-message' src={NewMessage} />
+            <audio id='my-message' src={MyMessage} />
         </div>
     )
 }
